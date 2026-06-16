8e472c939bd741e00d321bc2962be1a68d30e130
Simon <simonotaru14@gmail.com>
2026-06-16 21:54:04 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "./pages/Hero/Hero";
+import Experience from "./pages/Experience/Experience";
+
+const App = () => {
+  return (
+    <section>
+      <Hero />
+      <Experience />
+    </section>
+  );
+};
+
+export default App;
