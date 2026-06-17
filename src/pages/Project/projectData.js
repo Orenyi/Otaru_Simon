3238b924595d15e821c30aafff8d759294080f5e
Simon <simonotaru14@gmail.com>
2026-06-17 13:54:42 +0100

    Final touches

--- src/pages/Project/projectData.js
+++ src/pages/Project/projectData.js
@@ -1,6 +1,9 @@
 import otaruFit from "../../assets/projects/otaru_fit.webp";
 import dentalCare from "../../assets/projects/dental_care.webp";
 import resumeAI from "../../assets/projects/resume_ai.webp";
+import vima from "../../assets/projects/vima.webp";
+import timeApp from "../../assets/projects/timeapp.webp";
+import cleva from "../../assets/projects/cleva.webp";
 
 export const projects = [
   {
@@ -10,8 +13,8 @@ export const projects = [
       "AI-powered resume builder with templates, authentication, dashboard and PDF export.",
     image: resumeAI,
     tags: ["React.js", "Tailwind CSS", "Supabase"],
-    liveLink: "#",
-    githubLink: "#",
+    liveLink: "https://resume-aim-two.vercel.app",
+    githubLink: "https://github.com/Orenyi/Resume_ai",
   },
   {
     id: 2,
@@ -20,8 +23,8 @@ export const projects = [
       "Dental clinic website with online appointment booking and fully responsive layout.",
     image: dentalCare,
     tags: ["React.js", "Tailwind CSS"],
-    liveLink: "#",
-    githubLink: "#",
+    liveLink: "https://stellular-pavlova-a40969.netlify.app",
+    githubLink: "https://github.com/Orenyi/OTARU_dental_care",
   },
   {
     id: 3,
@@ -30,17 +33,37 @@ export const projects = [
       "Modern fitness website with animated sections, trainer showcases and responsive design.",
     image: otaruFit,
     tags: ["React.js", "Tailwind CSS"],
-    liveLink: "#",
-    githubLink: "#",
+    liveLink: "https://otaru-fit.netlify.app",
+    githubLink: "https://github.com/Orenyi/OTARU_fit",
   },
   {
     id: 4,
-    title: "OTARU FIT",
+    title: "Cleva",
+    description: "Cleva is a banking app, so I just recreated their homepage ",
+    image: cleva,
+    tags: ["HTML", "CSS", "JavaScript"],
+    liveLink: "https://tranquil-chaja-5d4398.netlify.app/",
+    githubLink: "https://github.com/Orenyi/Clevaprotype",
+  },
+
+  {
+    id: 5,
+    title: "TimeApp",
     description:
-      "Modern fitness website with animated sections, trainer showcases and responsive design.",
-    image: otaruFit,
+      "A responsive real-time clock web application built with React and Tailwind CSS",
+    image: timeApp,
     tags: ["React.js", "Tailwind CSS"],
-    liveLink: "#",
-    githubLink: "#",
+    liveLink: "https://timewebapp.netlify.app/",
+    githubLink: "https://github.com/Orenyi/TimeApp",
+  },
+
+  {
+    id: 6,
+    title: "Vima",
+    description:
+      "Get the Ultimate Nigerian TV Experience and More – Download Our App Now!",
+    image: vima,
+    tags: ["WordPress", "Elementor"],
+    liveLink: "https://dev-vima-ng.pantheonsite.io/",
   },
 ];
